c8eaa04d48f415958ec537bb4aea6079fcf7c9f4
MINEDUCYT <MINEDUCYT@DESKTOP-OAO2BUA>
2023-06-23 23:56:29 -0600

    Agregar archivos de proyecto.

---
+++ practica13.6.6/practica13.6.6/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace practica13._6._6
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
